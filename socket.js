758b9abfd239c1dfcddce6246636337bb188b430
Simon Cheek <simon.cheek@yahoo.com>
2024-02-27 12:32:15 -0700

    simulate web socketing

--- socket.js
+++ socket.js
@@ -1,4 +1,26 @@
-while (true) {
-    const popUp = document.createElement("div");
+const createPopUp = new Promise((res, rej) => {
 
-}
+    setInterval(
+        () => {
+
+            // create the div and manage
+            const newPopUp = document.createElement("div");
+            newPopUp.classList.add("pop-up");
+
+            const info = document.createElement("p");
+            info.style.marginBottom = "0";
+            info.innerText = `Friend${Math.floor(Math.random() * 1000)} added you to their list!`
+            newPopUp.appendChild(info);
+
+            const page = document.querySelector("main");
+            page.appendChild(newPopUp);
+
+            setTimeout(() => {
+                newPopUp.remove();
+            }, 4000)
+
+        }, 20000
+    )
+})
+
+
