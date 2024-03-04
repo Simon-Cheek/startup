901eeef3d4eb89e63be327ec6958e9fd7a4d2f01
Simon Cheek <simon.cheek@yahoo.com>
2024-03-04 16:45:36 -0700

    User creation moved to backend

--- index.js
+++ index.js
@@ -10,12 +10,37 @@ let apiRouter = express.Router();
 app.use('/api', apiRouter);
 
 
-// gets users array
+apiRouter.get('/users/all', (req, res) => {
+    res.send(users);
+})
+
+// gets specifc user
 apiRouter.get('/:name', (req, res) => {
     let name = req.params.name;
     let userName = findUser(name);
     res.send(userName);
 });
+
+
+// adds a user to the array
+apiRouter.post('/:user', (req, res) => {
+    let user = req.body;
+
+    // parse out prospective username to make sure username isn't already in use
+    let name = user.name;
+    let existingUser = findUser(name);
+
+
+    if (user.name && user.friends && user.goals && !existingUser) {
+
+        // push user to array of users
+        users.push(user);
+        res.send(users);
+    } else {
+        console.log("Tried to create invalid user!");
+        res.send([]);
+    }
+})
 
 
 
@@ -55,5 +80,5 @@ function findUser(name) {
     let user = users.find((u) => {
         return u.name == name;
     });
-    return user ? user : {};
+    return user ? user : null;
 }
