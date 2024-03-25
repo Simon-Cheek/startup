23ce22016e1aa0fa4005a41408eaa7cc85b0f3b5
Simon Cheek <simon.cheek@yahoo.com>
2024-03-25 17:48:23 -0600

    Fix launching issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const DB = require('./database.js');
 const WS = require('./websock.js');
+const { launchSock } = require('./websock.js');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const app = express();
@@ -176,6 +177,8 @@ app.use((req, res) => {
 
 
 
-app.listen(4000, () => {
+const httpServer = app.listen(4000, () => {
     console.log("Listening on port 4000!");
 });
+
+launchSock(httpServer);
--- websock.js
+++ websock.js
@@ -1,41 +1,54 @@
 const { WebSocketServer } = require('ws');
 const url = require('url');
 
-const wss = new WebSocketServer({ port: 9900 });
-
-const userConnections = new Map();
-
-// Handle User Connections and Add them to the Map (THERE MUST BE A USERNAME PASSED AS A HEADER)
-wss.on('connection', (ws, req) => {
-
-    const queryParams = url.parse(req.url, true).query;
-    const userName = queryParams['userName']; // Accessing the 'username' query parameter
+const wss = new WebSocketServer({ noServer: true });
 
 
-    if (!userName) {
-        console.error('Username not provided in the WebSocket URL.');
-        return ws.close();
-    }
+function launchSock(httpServer) {
 
-    const connections = userConnections.get(userName) || [];
-    connections.push(ws);
-    userConnections.set(userName, connections);
 
-    ws.on('close', () => {
-        // Remove closed WebSocket connection from the array of connections
-        const connections = userConnections.get(userName) || [];
-        const index = connections.indexOf(ws);
-        if (index !== -1) {
-            connections.splice(index, 1);
-            if (connections.length === 0) {
-                // If there are no more connections for this user, remove the entry from the map
-                userConnections.delete(userName);
-            } else {
-                userConnections.set(userName, connections);
-            }
-        }
+    // upgrade connections
+    httpServer.on('upgrade', (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
     });
-});
+
+    const userConnections = new Map();
+
+    // Handle User Connections and Add them to the Map (THERE MUST BE A USERNAME PASSED AS A HEADER)
+    wss.on('connection', (ws, req) => {
+
+        const queryParams = url.parse(req.url, true).query;
+        const userName = queryParams['userName']; // Accessing the 'username' query parameter
+
+
+        if (!userName) {
+            console.error('Username not provided in the WebSocket URL.');
+            return ws.close();
+        }
+
+        const connections = userConnections.get(userName) || [];
+        connections.push(ws);
+        userConnections.set(userName, connections);
+
+        ws.on('close', () => {
+            // Remove closed WebSocket connection from the array of connections
+            const connections = userConnections.get(userName) || [];
+            const index = connections.indexOf(ws);
+            if (index !== -1) {
+                connections.splice(index, 1);
+                if (connections.length === 0) {
+                    // If there are no more connections for this user, remove the entry from the map
+                    userConnections.delete(userName);
+                } else {
+                    userConnections.set(userName, connections);
+                }
+            }
+        });
+    });
+}
+
 
 // Function for telling users (each live connection of a single user) that someone added them as a friend
 function alertFriends(userName, friendUser) {
@@ -47,5 +60,6 @@ function alertFriends(userName, friendUser) {
 
 
 module.exports = {
-    alertFriends
+    alertFriends,
+    launchSock
 }
